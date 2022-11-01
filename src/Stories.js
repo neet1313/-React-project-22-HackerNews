4b79d27b80590c5eae757089a9281381ead85f52
Prabhneet Singh <prabhneet13@gmail.com>
2022-11-02 02:45:37 +0530

    Some content added

--- src/Stories.js
+++ src/Stories.js
@@ -3,7 +3,28 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  return <h2>stories component</h2>
+  const { isLoading, hits } = useGlobalContext();
+
+
+  if (isLoading) {
+    return <div className='loading' />
+  }
+
+  return <section className='stories'>
+    {hits.map(story => {
+      const { objectID, title, num_comments, author, url, points } = story;
+
+      return <article className='story' key={objectID}>
+        <h4 className='title'>{title}</h4>
+        <p className='info'>{points} points by <span>{author} | </span> {num_comments} comments</p>
+        <div>
+          <a href={url} className='read-link' target='_blank' rel="noopener noreferrer"> read more </a>
+
+          <button className='remove-btn'> remove </button>
+        </div>
+      </article>
+    })}
+  </section>
 }
 
 export default Stories
--- src/context.js
+++ src/context.js
@@ -11,12 +11,39 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  query: 'react',
+  page: 0,
+  hits: [],
+  nbPages: 0
+}
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+
+      dispatch({ type: SET_STORIES, payload: { hits: jsonData.hits, nbPages: jsonData.nbPages } })
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }
+
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, [])
+
+  return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
