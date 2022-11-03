78f66a9e8756d8b024968ae26d86e67971582719
Prabhneet Singh <prabhneet13@gmail.com>
2022-11-03 16:26:39 +0530

    More Functionalities added

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -14,7 +14,7 @@ const SearchForm = () => {
 
   return <form className='search-form' onSubmit={formSubmitHandler}>
     <h2>search hacker rank</h2>
-    <input type="text" className='form-input' onChange={inputChangeHandler} value={query} />
+    <input type="text" className='form-input' placeholder='Enter your search here' onChange={inputChangeHandler} value={query} />
   </form>
 }
 
--- src/context.js
+++ src/context.js
@@ -13,7 +13,7 @@ const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
 const initialState = {
   isLoading: true,
-  query: 'react',
+  query: ' ',
   page: 0,
   hits: [],
   nbPages: 0
@@ -52,7 +52,7 @@ const AppProvider = ({ children }) => {
 
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
-  }, [state.query])
+  }, [state.query, state.page])
 
   return <AppContext.Provider value={{ ...state, removeStoryHandler, searchQueryhandler, pageHandler }}>{children}</AppContext.Provider>
 }
